f7a473ecb8da6c9e28830dcf3a05182418107c3d
Andy Alonzo <andyalonzo@outlook.com>
2014-09-25 15:17:55 -0600

    Commit message

---
+++ app/src/main/java/com/soyalonzo/andy/clarosmsv2/MainApp.java
@@ -0,0 +1,69 @@
+package com.soyalonzo.andy.clarosmsv2;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+
+public class MainApp extends Activity {
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_app);
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        //webView.loadUrl("http://mensajes.claro.com.gt/mensaje_claro.php");
+        // Enable Javascript
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        //mWebView.loadUrl("http://claro.soyalonzo.com");
+        mWebView.loadUrl("file:///android_asset/claro.html");
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_app, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main_app, container, false);
+            return rootView;
+        }
+    }
+}
